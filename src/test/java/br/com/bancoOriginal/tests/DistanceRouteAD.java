4ba3f9538312622d7385462455c1c6435fed020f
DyeslenSilva <dyeslensilva97@gmail.com>
2021-07-24 20:08:39 -0300

    alterando casos de testes...

--- src/test/java/br/com/bancoOriginal/tests/DistanceRouteAD.java
+++ src/test/java/br/com/bancoOriginal/tests/DistanceRouteAD.java
@@ -1,29 +1,38 @@
+/**
+ * 
+ */
 package br.com.bancoOriginal.tests;
 
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
 
-import org.junit.Test;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.stubbing.Answer;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import br.com.bancoOriginal.grafo.model.RouteDistance;
 import br.com.bancoOriginal.grafo.model.RouteDistrict;
 import br.com.bancoOriginal.service.RoutesService;
+import junit.framework.Assert;
 
+/**
+ * @author Dyeslen
+ *
+ */
 @SpringBootTest
-public class DistanceRouteABC {
+class DistanceRouteAD {
 
 	@Mock
 	@InjectMocks
 	private RoutesService routeService;
 	
 	
-	@Test
-	public void testDistanceRouteABC(int maxStops,RouteDistance rdA,RouteDistance rdB, RouteDistrict rdC) {
-		when(routeService.avaliableRoutes(maxStops, rdA, rdB).add(rdC)).then((Answer<?>) routeService.getRouteDistricts());
+	
+	void distanceRouteAD(RouteDistance rdA, RouteDistance rdD,int maxStops) {
+		when(routeService.avaliableRoutes(maxStops, rdA, rdD)).thenReturn(routeService.getRouteDistricts());
 	}
 	
-
+		
 }

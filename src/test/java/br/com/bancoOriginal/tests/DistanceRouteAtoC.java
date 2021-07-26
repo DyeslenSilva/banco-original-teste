51e3ea89c425ae86a56d4975f7ef9524c2e26b24
DyeslenSilva <dyeslensilva97@gmail.com>
2021-07-26 11:41:54 -0300

    adicionando casos de testes...

--- src/test/java/br/com/bancoOriginal/tests/DistanceRouteAtoC.java
+++ src/test/java/br/com/bancoOriginal/tests/DistanceRouteAtoC.java
@@ -1,12 +1,13 @@
 package br.com.bancoOriginal.tests;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 
-import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.stubbing.Answer;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import br.com.bancoOriginal.grafo.model.RouteDistance;
 import br.com.bancoOriginal.service.RoutesService;
 
 @SpringBootTest
@@ -16,5 +17,24 @@ class DistanceRouteAtoC {
 	@InjectMocks
 	private RoutesService routesService;
 
+	
+	void routeToABC(int maxRoutes,RouteDistance routeDistance1, RouteDistance routeDistance2) {
+		when(routesService.avaliableRoutes(2, routeDistance1, routeDistance2)).then((Answer<?>) routesService.getRouteDistances());
+	}
+	
+	void routeToADC(RouteDistance routeDistance1, RouteDistance routeDistance2) {
+		when(routesService.avaliableRoutes(2, routeDistance1, routeDistance2)).then((Answer<?>) routesService.getRouteDistances());
+	}
 
+	
+	void routeToAEBC() {
+		
+	}
+	
+	void routeToAEDBC(RouteDistance routeDistance1, RouteDistance routeDistance2) {
+		when(routesService.avaliableRoutes(4, routeDistance1, routeDistance2)).then((Answer<?>) routesService.getRouteDistances());
+	}
+	
+	
+	
 }
--- src/test/java/br/com/bancoOriginal/tests/RouteDistanceCC.java
+++ src/test/java/br/com/bancoOriginal/tests/RouteDistanceCC.java
@@ -1,14 +1,31 @@
 package br.com.bancoOriginal.tests;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.stubbing.Answer;
+import org.springframework.boot.test.context.SpringBootTest;
 
+import br.com.bancoOriginal.grafo.model.RouteDistance;
+import br.com.bancoOriginal.service.RoutesService;
+
+@SpringBootTest
 class RouteDistanceCC {
 
-	@Test
-	void test() {
-		fail("Not yet implemented");
+	
+	@Mock
+	@InjectMocks
+	private RoutesService routesService;
+	
+	void testRouteDistanceCC(int maxStop,RouteDistance routeDistance1, RouteDistance routeDistance2) {
+		when(routesService.avaliableRoutes(maxStop, routeDistance1, routeDistance2)).then((Answer<?>) routesService.getRouteDistances());
+		;
 	}
+	
+	
+	
 
 }
